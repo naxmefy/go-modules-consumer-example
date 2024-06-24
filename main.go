16ad49db1c9704950e7e27d2b7475cc3a90c2608
MRW Neundorf <ninevillage@gmail.com>
2024-06-24 11:26:33 +0200

    feat: add github.com/naxmefy/go-modules/module1and2

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/naxmefy/go-modules/lib/a/b/c"
 	"github.com/naxmefy/go-modules/module1"
+	"github.com/naxmefy/go-modules/module1and2"
 	"github.com/naxmefy/go-modules/module2"
 )
 
 func main() {
 	module1.Hello()
 	module2.Hello()
+
+	fmt.Println("And now.......")
+
+	module1and2.Hello()
+
 	c.Hello()
 }
